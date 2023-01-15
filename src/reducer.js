975f166f0f18fbd063af04771b42b4eca1671776
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-01-16 00:15:16 +0200

    ✅ Prevent showing multiple 0

--- src/reducer.js
+++ src/reducer.js
@@ -9,6 +9,8 @@ export const ACTIONS = {
 export const reducer = (state, { type, payload }) => {
     switch (type) {
         case ACTIONS.ADD_DIGIT:
+            // Prevent showing multiple 0
+            if (payload.digit === "0" && state.current === "0") { return state }
             // Print "0." when type "."
             if (payload.digit === "." && state.current === undefined) { return { ...state, current: "0." } }
             return {
