f9f7c39c426b81b7f8db4439c7ccd09e774b9c75
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-01-15 23:45:40 +0200

    Build the reducer

---
+++ src/reducer.js
@@ -0,0 +1,14 @@
+export const ACTIONS = {
+    ADD_DIGIT: "add-digit",
+    CHOOSE_OPERATOR: "choose-operator",
+    CLEAR: "clear",
+    DELETE_DIGIT: "delete-digit",
+    EVALUATE: "evaluate",
+}
+
+export const reducer = (state, { type, payload }) => {
+    switch (type) {
+        default:
+            return { state }
+    }
+}
