fd4ad8ae8a6b51baec315d1eef8dee638cccfbc0
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-01-15 23:27:51 +0200

    💄 Build the console structure

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import "./App.css"
 function App() {
   return (
     <div className="Calc">
-      <div className="output"></div>
+      <div className="output">
+        {false ? (
+          <div className="result">155</div>
+        ) : (
+          <div>
+            <div className="previous">55 +</div>
+            <div className="current">100</div>{" "}
+          </div>
+        )}
+      </div>
       <div className="numberpad">
         <button className="span-two">AC</button>
         <button>Del</button>
